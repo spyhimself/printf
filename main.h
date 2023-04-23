0b6d08f41ceec6dcd8a94d77d07c56494a9d5c54
ayyoub <ayoublambaraachentih@gmail.com>
2023-04-22 21:46:18 -0700

    C-Printf

--- main.h
+++ main.h
@@ -1,11 +1,14 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
+#include <unistd.h>
 
 int _printf(const char *format, ...);
 int _putchar(char c);
-int _print_string(char *s);
+int _putstr(char *s);
+int _check(va_list list, char c);
 
 #endif
