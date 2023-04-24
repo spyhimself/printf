c0e7315f58da131d73486b82efedc357ca7ba806
ayyoub <ayoublambaraachentih@gmail.com>
2023-04-24 16:15:35 -0700

    C-Printf

--- convert_number.c
+++ convert_number.c
@@ -1,2 +1,110 @@
-ok
+#include "main.h"
 
+/**
+ * print_hex - unsigned hex numbers are printed in lowercase
+ * @ap: argument pointing
+ * @params: struct parameters
+ *
+ * Return: bytes written
+ */
+ 
+int print_hex(va_list ap, params_t *params)
+{
+	unsigned long l;
+	int c = 0;
+	char *str;
+
+	if (params->l_modifier)
+		l = (unsigned long)va_arg(ap, unsigned long);
+	else if (params->h_modifier)
+		l = (unsigned short int)va_arg(ap, unsigned int);
+	else
+		l = (unsigned int)va_arg(ap, unsigned int);
+
+	str = convert(l, 16, CONVERT_UNSIGNED | CONVERT_LOWERCASE, params);
+	if (params->hashtag_flag && l)
+	{
+		*--str = 'x';
+		*--str = '0';
+	}
+	params->unsign = 1;
+	return (c += print_number(str, params));
+}
+
+/**
+ * print_HEX - unsigned hex numbers are printed in uppercase
+ * @ap: parameter pointer
+ * @params: struct parameters
+ *
+ * Return: bytes printed
+ */
+ 
+int print_HEX(va_list ap, params_t *params)
+{
+	unsigned long l;
+	int c = 0;
+	char *str;
+
+	if (params->l_modifier)
+		l = (unsigned long)va_arg(ap, unsigned long);
+	else if (params->h_modifier)
+		l = (unsigned short int)va_arg(ap, unsigned int);
+	else
+		l = (unsigned int)va_arg(ap, unsigned int);
+
+	str = convert(l, 16, CONVERT_UNSIGNED, params);
+	if (params->hashtag_flag && l)
+	{
+		*--str = 'X';
+		*--str = '0';
+	}
+	params->unsign = 1;
+	return (c += print_number(str, params));
+}
+/**
+ * print_binary - generates an unsigned binary number
+ * @ap: argument pointing
+ * @params: the parameters structure
+ *
+ * Return: bytes printed
+ */
+ 
+int print_binary(va_list ap, params_t *params)
+{
+	unsigned int n = va_arg(ap, unsigned int);
+	char *str = convert(n, 2, CONVERT_UNSIGNED, params);
+	int c = 0;
+
+	if (params->hashtag_flag && n)
+		*--str = '0';
+	params->unsign = 1;
+	return (c += print_number(str, params));
+}
+
+/**
+ * print_octal - generates unsigned octal numbers
+ * @ap: argument pointing
+ * @params: the parameters structure
+ *
+ * Return: bytes printed
+ */
+ 
+int print_octal(va_list ap, params_t *params)
+{
+	unsigned long l;
+	char *str;
+	int c = 0;
+
+	if (params->l_modifier)
+		l = (unsigned long)va_arg(ap, unsigned long);
+	else if (params->h_modifier)
+		l = (unsigned short int)va_arg(ap, unsigned int);
+	else
+		l = (unsigned int)va_arg(ap, unsigned int);
+	str = convert(l, 8, CONVERT_UNSIGNED, params);
+
+	if (params->hashtag_flag && l)
+		*--str = '0';
+	params->unsign = 1;
+	return (c += print_number(str, params));
+}
