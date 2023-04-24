da3a7d9174f524a96a40edc1485ead50df3b0f8e
ayyoub <ayoublambaraachentih@gmail.com>
2023-04-23 22:10:02 -0700

    C-Printf

---
+++ print_functions.c
@@ -0,0 +1,137 @@
+#include "main.h"
+
+/**
+ * print_char - displays a character
+ * @ap: argumentative arrow
+ * @params: this structure's parameters
+ *
+ * Return: numbers written in char
+ */
+ 
+int print_char(va_list ap, params_t *params)
+{
+	char pad_char = ' ';
+	unsigned int pd = 1, sum = 0, ch = va_arg(ap, int);
+
+	if (params->minus_flag)
+		sum += _putchar(ch);
+	while (pd++ < params->width)
+		sum += _putchar(pd_char);
+	if (!params->minus_flag)
+		sum += _putchar(ch);
+	return (sum);
+}
+
+/**
+ * print_int - shows an integer
+ * @ap: argumentative pointer
+ * @params: this structure's parameters
+ *
+ * Return: numbers written in char
+ */
+ 
+int print_int(va_list ap, params_t *params)
+{
+	long l;
+
+	if (params->l_modifier)
+		l = va_arg(ap, long);
+	else if (params->h_modifier)
+		l = (short int)va_arg(ap, int);
+	else
+		l = (int)va_arg(ap, int);
+	return (print_number(convert(l, 10, 0, params), params));
+}
+
+/**
+ * print_string - outputs a string
+ * @ap: argumentative pointer
+ * @params: this structure's parameters
+ *
+ * Return: numbers written in char
+ */
+ 
+int print_string(va_list ap, params_t *params)
+{
+	char *str = va_arg(ap, char *), pad_char = ' ';
+	unsigned int pad = 0, sum = 0, i = 0, f;
+
+	(void)params;
+	switch ((int)(!str))
+		case 1:
+			str = NULL_STRING;
+
+    f = pad = _strlen(str);
+	if (params->precision < pad)
+		f = pad = params->precision;
+
+	if (params->minus_flag)
+	{
+		if (params->precision != UINT_MAX)
+			for (i = 0; i < pad; i++)
+				sum += _putchar(*str++);
+		else
+			sum += _puts(str);
+	}
+	while (f++ < params->width)
+		sum += _putchar(pad_char);
+	if (!params->minus_flag)
+	{
+		if (params->precision != UINT_MAX)
+			for (i = 0; i < pad; i++)
+				sum += _putchar(*str++);
+		else
+			sum += _puts(str);
+	}
+	return (sum);
+}
+
+/**
+ * print_percent - outputs a string
+ * @ap: argumentative pointer
+ * @params: the struct parameters
+ *
+ * Return: numbers written in char
+ */
+ 
+int print_percent(va_list ap, params_t *params)
+{
+	(void)ap;
+	(void)params;
+	return (_putchar('%'));
+}
+
+/**
+ * print_S - formatter for custom data
+ * @ap: argumentative pointer
+ * @params: this structure's parameters
+ *
+ * Return: printed numbers in chars
+ */
+ 
+int print_S(va_list ap, params_t *params)
+{
+	char *str = va_arg(ap, char *);
+	char *hex;
+	int sum = 0;
+
+	if ((int)(!str))
+		return (_puts(NULL_STRING));
+	for (; *str; str++)
+	{
+		if ((*str > 0 && *str < 32) || *str >= 127)
+		{
+			sum += _putchar('\\');
+			sum += _putchar('x');
+			hex = convert(*str, 16, 0, params);
+			if (!hex[1])
+				sum += _putchar('0');
+			sum += _puts(hex);
+		}
+		else
+		{
+			sum += _putchar(*str);
+		}
+	}
+	return (sum);
+}
