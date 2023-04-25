77fc54c0a688373b12ed3b721858e8f6d27d39ee
ayyoub <ayoublambaraachentih@gmail.com>
2023-04-24 17:27:06 -0700

    C-Printf

--- string_fields.c
+++ string_fields.c
@@ -1,3 +1,31 @@
+#include "main.h"
 
-kiok
-:wq
+/**
+ * get_precision - the precision is derived from the format string
+ * @p: formatting string
+ * @params: the structure of parameters
+ * @ap: argument pointing
+ *
+ * Return: new pointing device
+ */
+ 
+char *get_precision(char *p, params_t *params, va_list ap)
+{
+	int l = 0;
+
+	if (*p != '.')
+		return (p);
+	p++;
+	if (*p == '*')
+	{
+		l = va_arg(ap, int);
+		p++;
+	}
+	else
+	{
+		while (_isdigit(*p))
+			l = l * 10 + (*p++ - '0');
+	}
+	params->precision = l;
+	return (p);
+}
