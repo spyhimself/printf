9d5130c898a0a7a28fef2717ee34f4821d2bf702
ayyoub <ayoublambaraachentih@gmail.com>
2023-04-25 05:15:02 -0700

    C-Printf

--- _putchar.c
+++ _putchar.c
@@ -3,13 +3,40 @@
 /*Ayyoub Chentih && Aymen Tarar */
 
 /**
- * _putchar - stdout receives the character c
- * @c: The printed character
- * Return: Regarding success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * _puts - a string is printed with a newline
+ * @str: the string to be printed
+ *
+ * Return: void
  */
-
-int _putchar(char c)
+ 
+int _puts(char *str)
 {
-	return (write(1, &c, 1));
+	char *a = str;
+
+	while (*str)
+		_putchar(*str++);
+	return (str - a);
+}
+
+/**
+ * _putchar - stdout receives the character c
+ * @c: The character to be printed
+ *
+ * Return: On success 1.
+ * On error, The value -1 is returned, and errno is suitably set.
+ */
+ 
+int _putchar(int c)
+{
+	static int i;
+	static char buf[OUTPUT_BUF_SIZE];
+
+	if (c == BUF_FLUSH || i >= OUTPUT_BUF_SIZE)
+	{
+		write(1, buf, i);
+		i = 0;
+	}
+	if (c != BUF_FLUSH)
+		buf[i++] = c;
+	return (1);
 }
